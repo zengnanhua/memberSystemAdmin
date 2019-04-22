f854ebb1dce4da9431646d1cd51022e5afd23e15
nanhua <418852693@qq.com>
2019-04-22 23:46:06 +0800

    dfa

---
+++ BackSrc/AdminSystem.Api/Infrastructure/Migrations/20190422153915_InitApplicationContext.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdminSystem.Api.Infrastructure.Migrations
+{
+    public partial class InitApplicationContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    UserName = table.Column<string>(nullable: true),
+                    Pwd = table.Column<string>(nullable: true),
+                    Sex = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Address_Street = table.Column<string>(nullable: true),
+                    Address_City = table.Column<string>(nullable: true),
+                    Address_Province = table.Column<string>(nullable: true),
+                    Address_Country = table.Column<string>(nullable: true),
+                    Address_FullAddress = table.Column<string>(nullable: true),
+                    CreateUserId = table.Column<int>(nullable: false),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    UpdateUserId = table.Column<int>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: false),
+                    DeleteUserId = table.Column<int>(nullable: false),
+                    DeleteDateTime = table.Column<DateTime>(nullable: false),
+                    IsDelete = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MenuNo = table.Column<string>(nullable: true),
+                    PMenuNo = table.Column<string>(nullable: true),
+                    MenuName = table.Column<string>(nullable: true),
+                    Order = table.Column<string>(nullable: true),
+                    MenuUrl = table.Column<string>(nullable: true),
+                    MenuIcon = table.Column<string>(nullable: true),
+                    IsVisible = table.Column<bool>(nullable: false),
+                    DeepPath = table.Column<string>(nullable: true),
+                    PlatformType = table.Column<int>(nullable: false),
+                    MenuFuntionType = table.Column<int>(nullable: false),
+                    CreateUserId = table.Column<int>(nullable: false),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    UpdateUserId = table.Column<int>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: false),
+                    DeleteUserId = table.Column<int>(nullable: false),
+                    DeleteDateTime = table.Column<DateTime>(nullable: false),
+                    IsDelete = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    RoleName = table.Column<string>(nullable: true),
+                    RoleDescr = table.Column<string>(nullable: true),
+                    CreateUserId = table.Column<int>(nullable: false),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    UpdateUserId = table.Column<int>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: false),
+                    DeleteUserId = table.Column<int>(nullable: false),
+                    DeleteDateTime = table.Column<DateTime>(nullable: false),
+                    IsDelete = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRole",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false),
+                    RoleId = table.Column<int>(nullable: false),
+                    Id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRole", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_UserRole_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserRole_ApplicationUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "ApplicationUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUsers_Phone",
+                table: "ApplicationUsers",
+                column: "Phone",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUsers_UserName",
+                table: "ApplicationUsers",
+                column: "UserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRole_RoleId",
+                table: "UserRole",
+                column: "RoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Menus");
+
+            migrationBuilder.DropTable(
+                name: "UserRole");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "ApplicationUsers");
+        }
+    }
+}
