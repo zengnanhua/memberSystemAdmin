7f47e6026cacb76f228cc30ec9ff15f6a9fe9438
nanhua <418852693@qq.com>
2019-04-23 16:53:11 +0800

    添加单元测试

---
+++ BackSrc/test/unitTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using AdminSystem.Api.Controllers;
+using AdminSystem.Application.Commands;
+using MediatR;
+using Moq;
+using System;
+using System.Threading;
+
+namespace unitTest
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Mock<IMediator> _mediatorMock = new Mock<IMediator>();
+
+            _mediatorMock.Setup(x => x.Send(It.IsAny<CreateUserCommand>(), default(CancellationToken)));
+
+            HomeController homeController = new HomeController(_mediatorMock.Object);
+            var info= homeController.CreateUser().Result;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
