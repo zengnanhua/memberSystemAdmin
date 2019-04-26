3109916120639f980bfb7065559bde821b79860a
nanhua <418852693@qq.com>
2019-04-26 11:16:06 +0800

    跟新前端框架

---
+++ FontSrv_v2/src/views/dashboard/admin/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="dashboard-editor-container">
+      sdfasdf
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'DashboardAdmin',
+  components: {
+
+  },
+  data() {
+    return {
+     
+    }
+  },
+  methods: {
+    handleSetLineChartData(type) {
+     
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+
+  .github-corner {
+    position: absolute;
+    top: 0px;
+    border: 0;
+    right: 0;
+  }
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
+}
+</style>
