6fbe8c24ce0413835a6b4f82c07bd2d6253e121e
nanhua <418852693@qq.com>
2019-04-27 10:55:21 +0800

    提交修改

---
+++ FrontSrc/src/views/userManage/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <el-input v-model="test"></el-input>
+        <div style="height:2000px"></div>
+    </div>    
+</template>
+<script>
+export default {
+    name:"userManage",
+    data() {
+        return {
+            test:"",
+        }
+    },
+    mounted() {
+        
+    },
+    methods: {
+
+    }
+
+}
+</script>
+
